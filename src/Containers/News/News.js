e893f4df13fc0f111343de0a78ec31243492a4d8
Anderson Cossul <anderson@dexdigital.com.br>
2018-02-11 16:18:36 -0200

    Added autocomplete on input \o/

--- src/Containers/News/News.js
+++ src/Containers/News/News.js
@@ -1,59 +1,85 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import AutoCompleteInput from '../../Components/IO/AutoCompleteInput'
 import Cards from '../../Components/Cards/Cards'
 import styles from './News.css'
 
 class News extends Component {
   state = {
-    news: [],
+    news: null,
     hasError: false,
-    country: null,
-    search_query: '',
+    country_names_list: null,
+    country_name: null,
+    country_code: null,
     isLoading: false
   }
 
-  handleOnChange = (e) => {
-    this.setState({search_query: e.target.value})
+  componentDidMount () {
+    this.getCountryNames()
   }
 
-  handleOnSubmit = (e) => {
-    e.preventDefault()
-    this.setState({isLoading: true})
-    this.getCountryByName()
+  getCountryNames () {
+    axios.get('https://restcountries.eu/rest/v2/all')
+      .then((response) => {
+        const formattedList = response.data.map((country, index) => {
+          return {
+            id: index,
+            name: country.name
+          }
+        })
+        this.setState({country_names_list: formattedList})
+      })
+  }
+
+  onAutoCompleteInputSelectChanged = (selectedCountryName) => {
+    this.setState({
+      country_name: selectedCountryName,
+      news: null,
+      isLoading: true
+    }, () => {
+      this.getCountryByName()
+    })
   }
 
   getCountryByName () {
-    axios.get('https://restcountries.eu/rest/v2/name/' + this.state.search_query)
-      .then((response) => {
-        this.setState({country: response.data[0].alpha2Code}, () => {
-          this.getNews()
-        })
+    axios.get('https://restcountries.eu/rest/v2/name/' + this.state.country_name)
+    .then((response) => {
+      this.setState({country_code: response.data[0].alpha2Code}, () => {
+        this.getNews()
       })
-      .catch((error) => {
-        alert('Couldn\'t understand country.')
-      })
+    })
+    .catch((error) => {
+      alert('Couldn\'t understand country.')
+      this.setState({isLoading: false})
+    })
   }
 
   getNews () {
-    const { country } = this.state
-    axios.get('https://newsapi.org/v2/top-headlines?country=' + country + '&apiKey=a5e1edfa15894f9584c5d6e1d305851e')
-      .then((response) => {
-        const formattedNews = response.data.articles.map((article, index) => {
-          return {
-            id: index,
-            title: article.title,
-            author: article.author,
-            image: article.urlToImage,
-            url: article.url,
-            source: article.source.name,
-            date: article.publishedAt
-          }
-        })
-        this.setState({news: formattedNews})
+    const { country_code } = this.state
+    axios.get('https://newsapi.org/v2/top-headlines?country=' + country_code + '&apiKey=a5e1edfa15894f9584c5d6e1d305851e')
+    .then((response) => {
+      const formattedNews = response.data.articles.map((article, index) => {
+        return {
+          id: index,
+          title: article.title,
+          author: article.author,
+          image: article.urlToImage,
+          url: article.url,
+          source: article.source.name,
+          date: article.publishedAt
+        }
       })
-      .catch((error) => {
-        this.setState({hasError: true})
+      this.setState({
+        news: formattedNews,
+        isLoading: false
       })
+    })
+    .catch((error) => {
+      this.setState({
+        hasError: true,
+        isLoading: false
+      })
+    })
   }
 
   render () {
@@ -67,18 +93,22 @@ class News extends Component {
       news = <p>Loading...</p>
     }
 
-    if (this.state.news.length) {
-      news = <Cards data={this.state.news} />
+    if (this.state.news) {
+      if (this.state.news.length) {
+        news = <Cards data={this.state.news} />
+      } else {
+        news = <p>No articles found for {this.state.country_name}</p>
+      }
     }
 
     return (
       <div className={styles.News}>
-        <form onSubmit={this.handleOnSubmit.bind(this)}>
-          <input
-              type="text"
-              name="search_query"
-              onChange={this.handleOnChange.bind(this)}
-              placeholder="Type a Country"/>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <AutoCompleteInput
+            items={this.state.country_names_list}
+            onSelect={this.onAutoCompleteInputSelectChanged}
+            width_limit="100px"
+            placeholder="Type a Country"/>
         </form>
         {news}
       </div>
