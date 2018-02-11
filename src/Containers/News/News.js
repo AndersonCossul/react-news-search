ebc956a5dac10d13c60f9ee1ff282b4d724801af
Anderson Cossul <anderson@dexdigital.com.br>
2018-02-11 15:16:54 -0200

    Improvements here and there.

--- src/Containers/News/News.js
+++ src/Containers/News/News.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import Cards from '../../Components/Cards/Cards'
+import styles from './News.css'
 
 class News extends Component {
   state = {
-    news: null,
-    hasError: false
+    news: [],
+    hasError: false,
+    country: null,
+    search_query: '',
+    isLoading: false
+  }
+
+  handleOnChange = (e) => {
+    this.setState({search_query: e.target.value})
+  }
+
+  handleOnSubmit = (e) => {
+    e.preventDefault()
+    this.setState({isLoading: true})
+    this.getCountryByName()
+  }
+
+  getCountryByName () {
+    axios.get('https://restcountries.eu/rest/v2/name/' + this.state.search_query)
+      .then((response) => {
+        this.setState({country: response.data[0].alpha2Code}, () => {
+          this.getNews()
+        })
+      })
+      .catch((error) => {
+        alert('Couldn\'t understand country.')
+      })
   }
 
   getNews () {
-    axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=a5e1edfa15894f9584c5d6e1d305851e')
+    const { country } = this.state
+    axios.get('https://newsapi.org/v2/top-headlines?country=' + country + '&apiKey=a5e1edfa15894f9584c5d6e1d305851e')
       .then((response) => {
         const formattedNews = response.data.articles.map((article, index) => {
           return {
@@ -29,27 +56,30 @@ class News extends Component {
       })
   }
 
-  componentDidMount () {
-    this.getNews()
-  }
-
   render () {
-    let news = <p>Loading...</p>
+    let news = null
 
     if (this.state.hasError) {
       news = <p>There was an error when getting the news.</p>
     }
 
-    if (this.state.news) {
-      if (this.state.news.length) {
-        news = <Cards data={this.state.news} />
-      } else {
-        news = <p>No user was found!</p>
-      }
+    if (this.state.isLoading) {
+      news = <p>Loading...</p>
+    }
+
+    if (this.state.news.length) {
+      news = <Cards data={this.state.news} />
     }
 
     return (
-      <div>
+      <div className={styles.News}>
+        <form onSubmit={this.handleOnSubmit.bind(this)}>
+          <input
+              type="text"
+              name="search_query"
+              onChange={this.handleOnChange.bind(this)}
+              placeholder="Type a Country"/>
+        </form>
         {news}
       </div>
     )
