c11984a1c93bfb060560e8cb0c5dc91655138ef2
Anderson Cossul <anderson_cossul@hotmail.com>
2018-02-18 11:35:23 -0300

    Connected redux to the app.

--- src/index.js
+++ src/index.js
@@ -8,7 +8,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+import { Provider } from 'react-redux'
+import store from './redux/store'
 import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const Root = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
+
+ReactDOM.render(<Root />, document.getElementById('root'))
 registerServiceWorker()
