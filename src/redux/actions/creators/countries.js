96a1a505faa28ad04f86a80eac7d5c24dd8c4581
Anderson Cossul <anderson_cossul@hotmail.com>
2018-02-18 20:09:14 -0300

    Delete source to proper sync with github.

--- src/redux/actions/creators/countries.js
+++
@@ -1,8 +0,0 @@
-import * as actions from '../types'
-
-export const updateSelectedCountry = country => {
-  return {
-    type: actions.SET_SELECTED_AUTOCOMPLETE_COUNTRY,
-    country: country
-  }
-}
